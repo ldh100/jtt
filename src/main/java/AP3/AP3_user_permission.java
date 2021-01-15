a6214af7a0ff611359adcddcbd82f0792f6f1a81
Siva, Theleepan <Theleepan.Sivabalasingam@compassdigital.io>
2021-01-15 16:30:08 -0500

    Commit #3: Updated “AP3_user_permission” script: Increasing thread.sleep time from 500 to 1000. > Most of the time, scripts failing because the saving takes long time. > By increasing the sleep time, this issue is resolved.

--- src/main/java/AP3/AP3_user_permission.java
+++ src/main/java/AP3/AP3_user_permission.java
@@ -165,7 +165,7 @@ public class AP3_user_permission {
                         if (FAIL) { return; }  
                     _t++; Thread.sleep((long) sleep); T.Element_By_Path_Click("Click 'SAVE'", "xpath", "//button[contains(@class, 'mt-4 v-btn theme--light primary rounded m-2')]", "no_jira"); 
                         if (FAIL) { return; } // Save
-                    Thread.sleep(1000);  
+                    Thread.sleep(1000);    
                     _t++; Thread.sleep((long) sleep); T.Wait_For_All_Elements_InVisibility("Wait for save...", "xpath", "//*[contains(@class, 'v-progress-circular')]", "no_jira");
                         if (FAIL) { return;} 
                         
