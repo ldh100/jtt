e4a3e9d768bbd9480cb847f913dd013455633c65
Prathyusha Deshpande <prathyusha.deshpande@compassdigital.io>
2021-02-22 11:12:54 -0500

    Committed the code..

--- src/main/java/FW/FW_restaurants.java
+++ src/main/java/FW/FW_restaurants.java
@@ -13,6 +13,8 @@ import static FW.FW.*;
  */
 public class FW_restaurants {
     public static void run() throws InterruptedException { 
+        
+        //************************* Select a Restaurant Testcase*****************************************************************
         _t++; Thread.sleep((long) sleep); TWeb.Move_to_Element_By_Path("Open Dashboard Drawer", "xpath", "//nav[contains(@class, 'navigation-drawer')]", "no_jira");             
             if (FAIL) { return;}
         Thread.sleep(500);
@@ -38,7 +40,8 @@ public class FW_restaurants {
             }
         if(T_Index == -1) {
            _t++; _f++;
-           EX += " - " + "\t" + "Find Restaurant" + "\t" + RESTAURANT + "/" + RestMarket + "\t" + "Not Found" + "\t" + "FAIL" + "\t" + " - " + "\t" + " - " + "\r\n";            return;
+           EX += " - " + "\t" + "Find Restaurant" + "\t" + RESTAURANT + "/" + RestMarket + "\t" + "Not Found" + "\t" + "FAIL" + "\t" + " - " + "\t" + " - " + "\r\n";            
+           {return;}
         }   
         _t++; Thread.sleep((long) sleep); TWeb.Element_Click("Click target Restaurant/Market", L0.get(T_Index), "no_jira");
             if (FAIL) { return;}         
@@ -46,5 +49,69 @@ public class FW_restaurants {
         _t++; Thread.sleep((long) sleep); TWeb.Wait_For_All_Elements_InVisibility("Wait for 'progress'...", "xpath", "//div[@role='progressbar']", "no_jira"); 
             if (FAIL) { return;}
         _t++; Thread.sleep((long) sleep); TWeb.Find_Text("Find '" + RESTAURANT + "' Title Name", RESTAURANT, true,"no_jira");
-    }  
-}  
+        
+        
+        //*************************** Add a Restaurant Testcases**********************************
+         Thread.sleep(5000);
+        _t++; Thread.sleep((long) sleep); TWeb.Element_By_Path_Click("Click Drawer menu 'Restaurants'", "xpath", "//a[@href='/partners/']", "no_jira"); 
+            if (FAIL) { return;}
+         Thread.sleep(5000);
+        _t++; Thread.sleep((long) sleep); TWeb.Element_By_Path_Click("Click on the 'Add Restaurants'", "xpath", "(//*[@type='button'])[2]", "no_jira"); 
+            if (FAIL) { return;}
+        _t++; Thread.sleep((long) sleep); TWeb.Find_Text("Find 'Add a Restaurant' label", "Add a Restaurant", true,"no_jira"); 
+            if (FAIL) { return;}
+        _t++; Thread.sleep((long) sleep); TWeb.Element_E1_Find("Add a Restaurant form", "xpath", "(//div[@class='row justify-center'])[1]", "no_jira"); 
+            if (FAIL) { return;}        
+        _t++; Thread.sleep((long) sleep); TWeb.Element_Child_List_L1("Sections in Add a Restaurant form ", e1,"xpath", "//div/h5", "no_jira");             
+             if (FAIL) { return;}                              
+        for (int i = 1; i < L1.size(); i++) {
+            _t++; Thread.sleep((long) sleep); TWeb.Element_Text("Section---'"+i+"'", L1.get(i), "no_jira");                          
+                if (FAIL) { return;}
+    } 
+      _t++; Thread.sleep((long) sleep); TWeb.Element_Child_List_L1("Labels in Add a Restaurant form ", e1,"xpath", "(//*[contains(@class, 'v-label')])", "no_jira");             
+             if (FAIL) { return;}                              
+        for (int i = 0; i < L1.size(); i++) {
+            _t++; Thread.sleep((long) sleep); TWeb.Element_Text("Label---'"+i+"'", L1.get(i), "no_jira");                          
+                if (FAIL) { return;}
+    }   
+      _t++; Thread.sleep((long) sleep); TWeb.Element_By_Path_Text("Get the *FW Employee dropdown value ", "xpath", "(//*[contains(@class, 'v-select__selection')])[1]", "no_jira");
+      if (FAIL) { return;} 
+      
+  
+    _t++; Thread.sleep((long) sleep); TWeb.Element_By_Path_Click("*Market dropdown click ","xpath", "(//div[@class='v-input__icon v-input__icon--append'])[2]", "no_jira"); 
+      if (FAIL) { return;} 
+    _t++; Thread.sleep((long) sleep); TWeb.Element_Child_List_L1("*Market dropdown values ", e1,"xpath", "//div[@class='v-list-item__title']", "no_jira");  
+      if (FAIL) { return;}
+        for (int i = 2; i < L1.size(); i++) {
+            _t++; Thread.sleep((long) sleep); TWeb.Element_Text("*Market dropdown value---'"+i+"'", L1.get(i), "no_jira");                          
+                if (FAIL) { return;}
+          }  
+         _t++; Thread.sleep((long) sleep); TWeb.Element_Click("Select dropdown value", L1.get(4), "no_jira"); 
+            if (FAIL) { return;}
+
+    _t++; Thread.sleep((long) sleep); TWeb.Element_By_Path_Click("Partner Type dropdown click ","xpath", "(//div[@class='v-input__icon v-input__icon--append'])[3]", "no_jira"); 
+      if (FAIL) { return;} 
+    _t++; Thread.sleep((long) sleep); TWeb.Element_Child_List_L1("Partner Type dropdown values ", e1,"xpath", "//div[@class='v-list-item__title']", "no_jira");  
+      if (FAIL) { return;}
+        for (int i = 22; i < L1.size(); i++) {
+            _t++; Thread.sleep((long) sleep); TWeb.Element_Text("Partner Type dropdown value---'"+i+"'", L1.get(i), "no_jira");                          
+                if (FAIL) { return;}
+        }
+    _t++; Thread.sleep((long) sleep); TWeb.Element_Click("Select dropdown value", L1.get(22), "no_jira"); 
+    if (FAIL) { return;}  
+    
+    
+     _t++; Thread.sleep((long) sleep); TWeb.Element_By_Path_Click("*Restaurant Partner dropdown click ","xpath", "(//div[@class='v-input__icon v-input__icon--append'])[4]", "no_jira"); 
+      if (FAIL) { return;} 
+    _t++; Thread.sleep((long) sleep); TWeb.Element_Child_List_L1("*Restaurant Partner dropdown values ", e1,"xpath", "//div[@class='v-list-item__title']", "no_jira");  
+      if (FAIL) { return;}
+        for (int i = 24; i < L1.size(); i++) {
+            _t++; Thread.sleep((long) sleep); TWeb.Element_Text("*Restaurant Partner dropdown value---'"+i+"'", L1.get(i), "no_jira");                          
+                if (FAIL) { return;}
+        }
+    _t++; Thread.sleep((long) sleep); TWeb.Element_Click("Select dropdown value", L1.get(27), "no_jira"); 
+    if (FAIL) { return;}   
+    
+    
+} 
+}
