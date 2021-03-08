d91941764f1c9a0327b0640d5f9d37d3cb42d53a
oleg.spozito <oleg.spozito@603Q5S2.Canada.CompassGroup.Corp>
2021-03-07 20:49:58 -0500

    1.Android package development - WIP: - existed classes > updates/refactoring - new classes > An_Met.java, An_Mos.java 2. New "Jobs" (unattended execution) package - WIP - using new QA DB 'jobs' table - corresponding menu in main A GUI

---
+++ src/main/java/Android/An_bolter.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Android;
+import static Android.An_Mob.*;
+/**
+ *
+ * @author Oleg.Spozito
+ */
+public class An_bolter {
+    public static void run() throws InterruptedException {   
+//        Thread.sleep(500);
+//        _t++; Thread.sleep((long) sleep); Element_E1_Find("Is it Splash Screen/Logo?", "id", "splashScreenLogo", "no_jira");             
+//            //if (FAIL) { return;}        // "id", "packagename:id/splashScreenLogo",          
+//            
+        _t++; Thread.sleep((long) sleep); Wait_For_Element_By_Path_InVisibility("Wait for Splash screen", "id", "splashScreenLogo", "no_jira");             
+            if (FAIL) { return;} 
+        
+        _t++; Thread.sleep((long) sleep); Wait_For_Element_By_Path_Presence("Wait for 'Sign In' screen", "id", "button_login", "no_jira");             
+            if (FAIL) { return;} 
+        _t++; Thread.sleep((long) sleep); Element_By_Path_Text("Get 'Sign In' button text", "id", "button_login", "no_jira");             
+            if (FAIL) { return;} 
+        _t++; Thread.sleep((long) sleep); Element_By_Path_Text("Get 'Forgot PW?' button text", "id", "btn_forgotPassword", "no_jira");             
+            if (FAIL) { return;}      
+        _t++; Thread.sleep((long) sleep); Element_By_Path_Click("Click 'Email' input", "id", "textInputEditText_login_email_address", "no_jira");             
+            if (FAIL) { return;}            
+        _t++; Thread.sleep((long) sleep); Element_By_Path_Text_Enter("Enter 'Email'", "id", "textInputEditText_login_email_address", Bolter_ID, false,"no_jira");             
+            if (FAIL) { return;} 
+        _t++; Thread.sleep((long) sleep); Element_By_Path_Click("Click 'Password' input", "id", "textInputEditText_login_password", "no_jira");             
+            if (FAIL) { return;}            
+        _t++; Thread.sleep((long) sleep); Element_By_Path_Text_Enter("Enter 'Password' input", "id", "textInputEditText_login_password", Bolter_PW, true,"no_jira");             
+            if (FAIL) { return;} 
+        _t++; Thread.sleep((long) sleep); Element_By_Path_Click("Click 'Undide Password' icon", "id", "text_input_end_icon", "no_jira");             
+            if (FAIL) { return;}              
+        _t++; Thread.sleep((long) sleep); HideKeyboard("Hide Keyboard", "no_jira");             
+            if (FAIL) { return;}
+        _t++; Thread.sleep((long) sleep); Element_By_Path_Click("Click 'Log In' button", "id", "button_login", "no_jira");             
+            if (FAIL) { return;}  
+        _t++; Thread.sleep((long) sleep); Wait_For_Element_By_Path_InVisibility("Wait for load", "id", "android:id/progress", "no_jira");             
+            if (FAIL) { return;}          
+        _t++; Thread.sleep((long) sleep); Wait_For_Element_By_Path_Presence("Wait for 'Dashboard' screen", "id", "delivery_dashboard_title", "no_jira");             
+            if (FAIL) { return;} 
+
+        _t++; Thread.sleep((long) sleep); Element_By_Path_Click("Click 'Open Navigation drawer' image", "xpath", "//android.widget.ImageButton[@content-desc='Open navigation drawer']", "no_jira");             
+            if (FAIL) { return;} 
+        _t++; Thread.sleep((long) sleep); Element_By_Path_Text("Get 'About' > Version", "id", "appVersion","no_jira");             
+            if (FAIL) { return;}  
+        _t++; Thread.sleep((long) sleep); Element_By_Path_Click("Click 'Log out'", "id", "nav_logout", "no_jira");             
+            if (FAIL) { return;} 
+    }
+}
