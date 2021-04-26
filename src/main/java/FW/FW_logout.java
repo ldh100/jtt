07886631611ab2cbe3f6a35ce9757ace947e2f8a
Rajendram, Athetai <athetai.rajendram@compassdigital.io>
2021-04-26 17:33:46 -0400

    Updated code for logout file - Apr 26th (athe)

--- src/main/java/FW/FW_logout.java
+++ src/main/java/FW/FW_logout.java
@@ -15,12 +15,11 @@ import static WO.WO.*;
 public class FW_logout {
     public static void run() throws InterruptedException {   
         _t++; Thread.sleep((long) sleep); TWeb.Move_to_Element_By_Path("Open Dashboard Drawer", "xpath", "//nav[contains(@class, 'navigation-drawer')]", "no_jira");             
-            if (FAIL) { return;}
-        Thread.sleep(500);
-         _t++; Thread.sleep((long) sleep);TWeb.Element_By_Path_Click("Click signout", "xpath","//*[@class='Caption-bold']", "no_jira");
+        _t++; Thread.sleep((long) sleep); TWeb.Find_Text("FW manager's  Dashboard 'Log out'", "Log out", true,"no_jira"); 
+        _t++; Thread.sleep((long) sleep);TWeb.Element_By_Path_Click("Click on Log out", "xpath","//*[@class='Caption-bold']", "no_jira");
          
         _t++; Thread.sleep((long) sleep);TWeb.Element_By_Path_Text("Sign into FOODWORKS Portal", "xpath", "//h3[contains(text(),'Sign into FOODWORKS Portal')]", "no_jira");
-        Thread.sleep(500);
+   
         
     }
 }
