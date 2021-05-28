10d9686dc5faff20da897dbd2df2ac3a0d970d01
Oleg.Spozito <Oleg.Spozito@603Q5S2.Canada.CompassGroup.Corp>
2021-05-27 22:41:15 -0400

    1. Updates in DL, C360 2. Refactoring AP3 into AP3_New - WIP

--- src/main/java/AP3/AP3_login.java
+++ src/main/java/AP3/AP3_login.java
@@ -1,17 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package AP3;
 import A.TWeb;
 import static A.A.*;
 import static AP3.AP3.*;
-/**
- *
- * @author Oleg.Spozito
- */
-public class AP3_login {
+
+public class AP3_login{
     public static void run(boolean users) throws InterruptedException {     
         _t++; Thread.sleep((long) sleep); TWeb.Navigate_to_URL("Navigate to", url,"no_jira");             
             if (FAIL) { return;}
--- src/main/java/DL/DL_insights.java
+++ src/main/java/DL/DL_insights.java
@@ -2,7 +2,7 @@
 package DL;
 
 
-public class DL_insights extends DL_GUI {
+class DL_insights extends DL_GUI {
     protected DL_insights(DL_GUI a) {
         DL_UserID = a.DL_UserID;
         DL_UserPW = a.DL_UserPW;
