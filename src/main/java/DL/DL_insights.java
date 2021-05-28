9717d6ae5ea80bff7374bea432a137b2222d8d02
Prathyusha Deshpande <prathyusha.deshpande@compassdigital.io>
2021-05-28 15:56:58 -0400

    WIP of Insights

--- src/main/java/DL/DL_insights.java
+++ src/main/java/DL/DL_insights.java
@@ -1,4 +1,3 @@
-
 package DL;
 
 
@@ -13,6 +12,17 @@ public class DL_insights extends DL_GUI {
         ParentTest = a.ParentTest;
     }
     protected void run() throws InterruptedException, Exception { 
-        // ====
+        Thread.sleep(500);
+        Wait_For_All_Elements_InVisibility("Wait for 'progress'...", "xpath", "//*[contains(@class, 'progress')]", ParentTest, "no_jira");
+        if (FAIL) {
+            return;
+        }
+        Thread.sleep(500);
+        Find_Text("Find 'Insights' label", "Insights", true, ParentTest, "no_jira");
+        Move_to_Element_By_Path("User is able to find 'Insights' label", "xpath", "//p[contains(text(),'Insights')]", ParentTest, "no_jira");
+        List_L0("Get Insights tab Count", "xpath", "//span[contains(@class, 'MuiTab-wrapper')]", ParentTest, "no_jira");
+        if (FAIL) {
+            return;
+        }
     }  
 }
