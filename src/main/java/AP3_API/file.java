8e8a3cd08d981c251c8c0e61d52fc64bc28c82be
oleg.spozito <oleg.spozito@10.0.0.227>
2022-07-06 11:13:50 -0400

    AP3_API > /file/: added Successful POST, DELETE API(s)

--- src/main/java/AP3_API/file.java
+++ src/main/java/AP3_API/file.java
@@ -1,5 +1,8 @@
 package AP3_API;
 
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Base64;
 import org.json.JSONObject;
 
@@ -18,7 +21,17 @@ class file extends AP3_API_GUI{
     JSONObject requestParams = new JSONObject();
     String AAA = "";
     String File_Base64 = "";
+
+
     protected void run() { 
+        String File_Path = System.getProperty("user.dir") + File.separator + "FilesToUpload" + File.separator + "AP3_brand_image.png"; 
+        try{
+            byte[] File_Bytes = Files.readAllBytes(Paths.get(File_Path));
+            File_Base64 = Base64.getEncoder().encodeToString(File_Bytes);
+        }catch(Exception ex){
+            AAA = ex.getMessage();
+        }
+
         Auth = "";
         BODY = "";
         JOB_Api_Call("Upload a file to CDL S3 - Unauthorized", "POST", 
@@ -27,8 +40,7 @@ class file extends AP3_API_GUI{
             AAA = json.toString(4);
         } 
         
-        Auth = "Bearer " + AP3_TKN;
-        File_Base64 = Base64.getEncoder().encodeToString(("TestFile_" + NewID + "\n" + "Line 1").getBytes());
+        Auth = "Bearer " + AP3_TKN;        
         requestParams = new JSONObject();
         requestParams.put("file_base64", File_Base64);
         requestParams.put("file_name", "TestFile_" + NewID + "_Please_Delete.txt");
@@ -50,10 +62,10 @@ class file extends AP3_API_GUI{
         if(json != null){
             AAA = json.toString(4);
         } 
-        
-        File_Base64 = "TestFile_" + NewID + "\n" + "Line 1";
+
+        String Wrong_File_Base64 = "TestFile_" + NewID + "\n" + "Line 1";
         requestParams = new JSONObject();
-        requestParams.put("file_base64", File_Base64);
+        requestParams.put("file_base64", Wrong_File_Base64);
         requestParams.put("file_name", "TestFile_" + NewID + "_Please_Delete.txt");
         //requestParams.put("bucket_name", "Invalid_Bucket"); 
         BODY = requestParams.toString();
@@ -62,9 +74,28 @@ class file extends AP3_API_GUI{
         if(json != null){
             AAA = json.toString(4);
         }   
-        
-        if (!env.equals("PR")) {
-            // Uplod file
-        }        
+
+        String New_S3_File_URL = "";
+        requestParams = new JSONObject();
+        requestParams.put("file_base64", File_Base64);
+        requestParams.put("file_name", "TestFile_" + NewID + "_Please_Delete.txt");
+        requestParams.put("bucket_name", "images.compassdigital.org"); 
+        BODY = requestParams.toString();
+        JOB_Api_Call("Upload a file to CDL S3 - Expect OK", "POST", 
+            BaseAPI + "/file", Auth, BODY, 200, ParentTest, "no_jira"); 
+        if(json != null){
+            AAA = json.toString(4);
+            New_S3_File_URL = json.getString("url");
+        }   
+  
+        requestParams = new JSONObject();
+        requestParams.put("bucket_name", "images.compassdigital.org"); 
+        requestParams.put("file_path", New_S3_File_URL);
+        BODY = requestParams.toString();
+        JOB_Api_Call("Delete uploaded file from CDL S3 - Expect OK", "DELETE", 
+            BaseAPI + "/file", Auth, BODY, 200, ParentTest, "no_jira"); 
+        if(json != null){
+            AAA = json.toString(4);
+        } 
     }
 }
